5904707640e101a3868b6434e43b0497e7794c92
indunil-jay <indunil.jayz@gmail.com>
2024-06-08 11:45:08 +0530

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
+import AppGuide from "./pages/AppGuide";
+import PageNotFound from "./pages/PageNotFound";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/how-to-use" element={<AppGuide />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
